9bc3d72d5b103a0e7af132c9f37cdb616583ad37
Mario Medina <mario@mariomedina.net>
2024-12-12 17:50:25 -0500

    Removing old dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,8 @@
 #include <Arduino.h>
-#include <painlessMesh.h>
 #include <esp_mesh_now.h>
 
-#define MESH_PREFIX "whateverYouLike"
-#define MESH_PASSWORD "somethingSneaky"
-#define MESH_PORT 5555
-
-Scheduler userScheduler; // to control your personal task
-painlessMesh mesh;
 espMeshNow_t espMeshNow;
-// User stub
-void sendMessage(); // Prototype so PlatformIO doesn't complain
 
-Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
-
-void sendMessage()
-{
-  String msg = "Hi from node1";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
-}
-
-// Needed for painless library
 void receivedCallback(uint64_t from, String msg)
 {
   Serial.printf("startHere: Received from %llX msg=%s\n", from, msg.c_str());
@@ -45,32 +25,6 @@ void newConnectionCallback(uint32_t nodeId)
 void changedConnectionCallback()
 {
   Serial.printf("Changed connections\n");
-}
-
-void nodeTimeAdjustedCallback(int32_t offset)
-{
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
-
-void taskMeshUpdate(void *params)
-{
-  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
-
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT, WIFI_STA);
-  // mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
-
-  userScheduler.addTask(taskSendMessage);
-  taskSendMessage.enable();
-  while (true)
-  {
-    // it will run the user scheduler as well
-    mesh.update();
-    taskYIELD();
-  }
 }
 
 void setup()
